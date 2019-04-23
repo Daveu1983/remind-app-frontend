1a17b750112558c43926ef802b03422ee0d3ab25
David Underwood <dunderwood13@hotmail.co.uk>
2019-04-23 13:06:13 +0100

    made task plural depending on total number of tasks

--- src/components/SummaryOfItems.js
+++ src/components/SummaryOfItems.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 
-let totalTasks = 4;;
+let totalTasks = 5;
 let dueSoonTasks = 3;
 let overdueTasks = 1;
+let wordTask = "tasks";
+
+if (totalTasks === 1){
+    wordTask = "task";
+}
 
 class SummaryOfItems extends Component {
   render() {
-    if (totalTasks >1){    
+    if (totalTasks > 0){    
         return (
             <div className="container">
                 <div class="row generalText">
                     <div className="col-12">
-                        <p>You have {totalTasks} tasks, {dueSoonTasks} due within the next 24hrs
-                        and {overdueTasks} overdue task </p>
+                        <p>You have {totalTasks} {wordTask}, {dueSoonTasks} due within the next 24hrs
+                        and {overdueTasks} overdue</p>
                     </div>
                 </div>
             </div>
