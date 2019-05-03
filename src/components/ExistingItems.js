3380e1428389fda12593ff12a85414f458ac0b61
David Underwood <dunderwood13@hotmail.co.uk>
2019-05-03 12:40:20 +0100

    removed display of ID tag - no need for it to be diplayed to the user

--- src/components/ExistingItems.js
+++ src/components/ExistingItems.js
@@ -10,7 +10,6 @@ class ExistingItems extends Component {
       <div className={`container ${!this.props.showDeleted && this.props.itemDeleted ? "hidden" : ""}`}>
         <div className="row generalContent">
             <div className="col-md-7 col-12 text-center">Task Description {this.props.itemDescription}</div>
-            <div className="col-md-1 col-12 text-center">Task ID {this.props.itemID}</div>
             <div className="col-md-2 col-12 text-center"><button className="btn btn-secondary"  
                 type="button">modify</button></div>
             <div className="col-md-2 col-12 text-center"><button className="btn btn-secondary" 
